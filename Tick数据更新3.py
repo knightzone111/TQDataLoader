a46babc465707423263424842be142bc0ac57a47
kztrader-linux <kzchina@outlook.com>
2020-09-21 20:06:48 +0800

    update

---
+++ Tick数据更新3.py
@@ -0,0 +1,15 @@
+from tqsdk import TqApi
+from DataLoader import download_data, read_data
+from datetime import datetime
+
+api = TqApi()
+quotes = api._data['quotes']
+
+symbols = []
+for symbol, item in quotes.items():
+    if item['ins_class'] == 'FUTURE':
+        symbols.append(symbol)
+
+download_data(symbols[1000:1500], datetime(2016,1,1), datetime(2020,8,25), "tick")
+#download_data(symbols, datetime(2016,1,1), datetime(2020,8,25), "D")
+
---
+++ Tick数据更新4.py
@@ -0,0 +1,15 @@
+from tqsdk import TqApi
+from DataLoader import download_data, read_data
+from datetime import datetime
+
+api = TqApi()
+quotes = api._data['quotes']
+
+symbols = []
+for symbol, item in quotes.items():
+    if item['ins_class'] == 'FUTURE':
+        symbols.append(symbol)
+
+download_data(symbols[2500:3000], datetime(2016,1,1), datetime(2020,8,31), "tick")
+#download_data(symbols, datetime(2016,1,1), datetime(2020,8,25), "D")
+
